471d8204b7c3675a86b6baf166fe7c78a8720340
kenne <kennethnnadi0@outlook.com>
2020-02-19 10:26:25 +0000

    first commit

--- SwimmingPool/src/test/Test.java
+++ SwimmingPool/src/test/Test.java
@@ -6,7 +6,7 @@ public class Test {
 		System.out.println("Test one - Stephen");
 		System.out.println("Test two - Joyce");
 		System.out.println("Test two - Edwina");
-		
+		System.out.println("test three kenneth");
 	}
 
 }
