47ba42b03f926cafeb4dd6f97fa4a6c9405a0e55
edwina <esammon@ait.ie>
2020-02-19 10:15:42 +0000

    test 5

--- SwimmingPool/src/test/Test.java
+++ SwimmingPool/src/test/Test.java
@@ -5,6 +5,8 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Test one - Stephen");
 		System.out.println("Test two - Joyce");
+		System.out.println("Test two - Edwina");
+		
 	}
 
 }
