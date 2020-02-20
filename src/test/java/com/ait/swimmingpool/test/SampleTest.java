a70d990f006627153f3aca3dc51c66dfb58ba8a2
Joyce <joyce.apsmartins@gmail.com>
2020-02-20 00:05:16 +0000

    Adding basic structures

---
+++ src/test/java/com/ait/swimmingpool/test/SampleTest.java
@@ -0,0 +1,16 @@
+package com.ait.swimmingpool.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SampleTest {
+
+	@Test
+	public void test() {
+		assertEquals("Numbers should match", 1, 1);
+	}
+
+}
