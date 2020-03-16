ba605a9d1b294c799f9f545e189e6b91a8a63592
Stephen <stephenscreeney1@gmail.com>
2020-03-16 10:49:09 +0000

    Update(PaymentDAO, PaymentResource)

--- src/main/java/com/ait/swimmingpool/dao/PaymentDAO.java
+++ src/main/java/com/ait/swimmingpool/dao/PaymentDAO.java
@@ -1,6 +1,7 @@
 package com.ait.swimmingpool.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -8,7 +9,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.ait.swimmingpool.bean.PaymentBean;
-import com.ait.swimmingpool.bean.UserBean;
 import com.ait.swimmingpool.dbconnection.ConnectionHelper;
 
 public class PaymentDAO {
@@ -39,5 +39,68 @@ public class PaymentDAO {
 		payment.setAmount(rs.getDouble("amount"));
 		payment.setPaymentDate(rs.getString("payment_date"));
 		return payment;
+	}
+	
+	public PaymentBean create(PaymentBean payment) {
+		Connection c = null;
+		PreparedStatement ps = null;
+		try {
+			c = ConnectionHelper.getConnection();
+			ps = c.prepareStatement("INSERT INTO payment" +
+			" (user_id, amount, payment_date)" +
+			" VALUES (?, ?, ?)",
+			new String[] { "payment_id" });
+			ps.setString(1, payment.getUserId());
+			ps.setDouble(2, payment.getAmount());
+			ps.setString(3, payment.getPaymentDate());
+			ps.executeUpdate();
+			ResultSet rs = ps.getGeneratedKeys();
+			rs.next();
+			// Update the id in the returned object. This is important as this value must be returned
+			int id = rs.getInt(1);
+			payment.setPaymentId(id);;
+		}catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}finally {
+			ConnectionHelper.close(c);
+		}
+		return payment;
+	}
+	
+	public PaymentBean update(PaymentBean payment) {
+		Connection c = null;
+		try {
+			c = ConnectionHelper.getConnection();
+			PreparedStatement ps = c.prepareStatement("UPDATE Payment SET user_id = ?, amount = ?, payment_date = ? " +
+			"WHERE payment_id = ?");
+			ps.setString(1, payment.getUserId());
+			ps.setDouble(2, payment.getAmount());
+			ps.setString(3, payment.getPaymentDate());
+			ps.setInt(4, payment.getPaymentId());
+			ps.executeUpdate();
+		}catch(SQLException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}finally {
+			ConnectionHelper.close(c);
+		}
+		return payment;
+	}
+	
+	public boolean remove(int id) {
+		Connection c = null;
+		try {
+			c = ConnectionHelper.getConnection();
+			PreparedStatement ps = c.prepareStatement("DELETE FROM Payment WHERE payment_id=?");
+			ps.setInt(1, id);
+			int count = ps.executeUpdate();
+			return count == 1;
+		}catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}finally {
+			ConnectionHelper.close(c);
+		}
 	}
 }
--- src/main/java/com/ait/swimmingpool/resources/PaymentResource.java
+++ src/main/java/com/ait/swimmingpool/resources/PaymentResource.java
@@ -1,5 +1,52 @@
 package com.ait.swimmingpool.resources;
 
-public class PaymentResource {
+import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import com.ait.swimmingpool.bean.PaymentBean;
+import com.ait.swimmingpool.dao.PaymentDAO;
+
+@Path("/payment")
+public class PaymentResource {
+	PaymentDAO dao = new PaymentDAO();
+	
+	@GET
+	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+	public List<PaymentBean> findAll(){
+		System.out.println("findAll");
+		return dao.findAll();
+	}
+	
+	@POST
+	@Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+	public PaymentBean create(PaymentBean payment) {
+		System.out.println("creating payment");
+		return dao.create(payment);
+	}
+	
+	@PUT @Path("{id}")
+	@Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+	public PaymentBean update(PaymentBean payment) {
+		System.out.println("Updating payment");
+		dao.update(payment);
+		return payment;
+	}
+	
+	@DELETE @Path("{id}")
+	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+	public void remove(@PathParam("id") int id) {
+		dao.remove(id);
+	}
 }
