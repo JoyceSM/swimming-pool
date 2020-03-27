f69d7555c9b3ce031f3787fcf6b37e6de06ddd1f
Joyce <joyce.apsmartins@gmail.com>
2020-03-27 23:01:17 +0000

    Update Login (Roles Page, Class, index)

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -1,27 +1,27 @@
-var rootURL = "http://localhost:8080/swimming-pool/rest/login";
-
-var loginVar = localStorage.getItem('loginVar');
-
 $(document).ready(function() {
 
-	$('#btnSignOut').hide();
+	var rootURL = "http://localhost:8080/swimming-pool/rest/login";
+
+	var loginVar = localStorage.getItem('loginVar');
+
+	$('#menu-logout').hide();
+	$('#menu-login').hide();
 
 	if (loginVar == 1) {
-		$("#loginContainer").html("<h1>Manager</h1>");
-		console.log("Manager");
+		$('#manager').show();
 	} else if (loginVar == 2) {
-		console.log("Staff");
-		$("#loginContainer").html("<h1>Staff</h1>");
+		$('#staff').show();
 	} else if (loginVar == 3) {
-		console.log("Customer");
-		$("#loginContainer").html("<h1>Customer</h1>");
+		$('#customer').show();
 	}
 
-	if (loginVar != 0) {
-		$('#btnSignOut').show();
+	if (loginVar != 0 && loginVar != null) {
+		$('#menu-logout').show();
+		$('#loginContainer').hide();
+		$('#menu-login').hide();
 	}
 
-	$('#btnSignOut').click(function() {
+	$('#menu-logout').click(function() {
 		logout();
 		return false;
 	});
@@ -34,49 +34,54 @@ $(document).ready(function() {
 		login(username, password);
 		return false;
 	})
-});
-// create findById
-function findByUsername(username, password) {
-	var loginData;
-	console.log('findByUsername: ' + username);
-	$.ajax({
-		type : 'GET',
-		url : rootURL + '?username=' + username + '&password=' + password,
 
-		dataType : "json",
-		async : false,
-		success : function(data) {
-			loginData = data
-		},
-		error : function(XMLHttpRequest, textStatus, errorThrown) {
-			console.log("User doesn't exist error")
-		}
-	});
-	return loginData;
-};
-// create login function
-function login(username, password) {
+	// create findById
+	function findByUsername(username, password) {
+		var loginData;
+		console.log('findByUsername: ' + username);
+		$.ajax({
+			type : 'GET',
+			url : rootURL + '?username=' + username + '&password=' + password,
 
-	if (!username || !password) {
+			dataType : "json",
+			async : false,
+			success : function(data) {
+				loginData = data
+			},
+			error : function(XMLHttpRequest, textStatus, errorThrown) {
+				console.log("User doesn't exist error")
+			}
+		});
+		return loginData;
+	}
+	;
+	// create login function
+	function login(username, password) {
 
-	} else {
-		accessType = findByUsername(username, password);
-		if (accessType != 0) {
-			localStorage.setItem('loginVar', accessType);
-			window.location.reload();
+		if (!username || !password) {
+
+		} else {
+			accessType = findByUsername(username, password);
+			if (accessType != 0) {
+				localStorage.setItem('loginVar', accessType);
+				window.location.reload();
+
+			}
 		}
 	}
-}
-// create clear field
-function clearUsernameAndPasswordField() {
-	$('#inputUsername').val('');
-	$('#inputPassword').val('');
-};
-// create logout
-function logout() {
-	clearUsernameAndPasswordField();
+	// create clear field
+	function clearUsernameAndPasswordField() {
+		$('#inputUsername').val('');
+		$('#inputPassword').val('');
+	}
+	;
+	// create logout
+	function logout() {
+		clearUsernameAndPasswordField();
 
-	loginVar = 0;
-	localStorage.setItem('loginVar', 0);
-	window.location.reload(true);
-};
+		loginVar = 0;
+		localStorage.setItem('loginVar', 0);
+		window.location.assign("loginPage.html");
+	}
+	;
+});
